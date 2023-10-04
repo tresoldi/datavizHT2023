d361a1dcd2d2248bbc841fb7da141774bb8322a6
Tiago Tresoldi <tiago.tresoldi@lingfil.uu.se>
2023-10-04 16:38:51 +0200

    Update gdp_vs_lifeExp_2007.R

--- gdp_vs_lifeExp_2007.R
+++ gdp_vs_lifeExp_2007.R
@@ -1,11 +1,15 @@
+# add comments
+
 library(gapminder)
 library(ggplot2)
+library(tidyverse)
+library(tidytext)
 
-gapminder_2005 <- subset(gapminder, year == 2005)
+gapminder_2000 <- subset(gapminder, year == 2000)
 
-ggplot(gapminder_2005, aes(x=log(gdpPercap), y=lifeExp, color=continent)) +
+ggplot(gapminder_2000, aes(x=log(gdpPercap), y=lifeExp, color=continent)) +
   geom_point(aes(size=pop), alpha=0.7) +
-  labs(title="GDP per capita vs. Life Expectancy (2005)",
+  labs(title="GDP per capita vs. Life Expectancy (2000)",
        x="Log of GDP per capita",
        y="Life expectancy",
        subtitle="Data from Gapminder",
