1b743bde15b511d07262f72cf8adc7844163574d
Biyanto <biyanto.rebin@gmail.com>
2023-10-04 16:20:30 +0200

    Update gdp_vs_lifeExp_2007.R

--- gdp_vs_lifeExp_2007.R
+++ gdp_vs_lifeExp_2007.R
@@ -7,7 +7,7 @@ ggplot(gapminder_2007, aes(x=log(gdpPercap), y=lifeExp, color=continent)) +
   geom_point(aes(size=pop), alpha=0.7) +
   labs(title="GDP per capita vs. Life Expectancy (2007)",
        x="Log of GDP per capita",
-       y="Life Expectancy",
+       y="Life expectancy",
        subtitle="Data from Gapminder",
        caption="Colored by continent") +
   theme_minimal()
